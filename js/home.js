46cbf754adf4a8673a0f2cfbc8eb655140876468
Everton Barth <e.barth.dev@gmail.com>
2026-05-18 21:41:51 -0300

    Removed the script that injects the style to the Homepage.

--- js/home.js
+++ js/home.js
@@ -1,20 +1,3 @@
-function addHomeStylesheet() {
-  const href = "https://cdn.jsdelivr.net/gh/e-barth/winning-edge-real-estate/css/home.min.css";
-  function inject() {
-    if (!document.head) {
-      return setTimeout(inject, 100);
-    }   
-    if (!document.querySelector(`link[href="${href}"]`)) {
-      const link = document.createElement("link");
-      link.rel = "stylesheet";
-      link.href = href;
-      document.head.appendChild(link);
-    }
-  }
-  inject();
-}
-addHomeStylesheet();
-
 function moveContent1() {
   const target = document.querySelector('.banner-button-v2');
   const content = document.querySelector('.md-search-more-center');
